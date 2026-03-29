522826eb314044d70e850bc5ad0af5e1aa448618
Yoswell <csyhnu@163.com>
2026-03-30 02:57:22 +0800

    optimize: 完善鉴权流程

--- src/main/java/com/yoswell/agenticrag/web/security/util/SecurityUtils.java
+++ src/main/java/com/yoswell/agenticrag/web/security/util/SecurityUtils.java
@@ -3,6 +3,8 @@ package com.yoswell.agenticrag.web.security.util;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 
+import com.yoswell.agenticrag.common.exception.BusinessException;
+import com.yoswell.agenticrag.common.exception.ErrorCode;
 import com.yoswell.agenticrag.web.security.model.TenantUser;
 
 import reactor.core.publisher.Mono;
@@ -23,7 +25,7 @@ public final class SecurityUtils {
                     }
                     return authentication.getName();
                 })
-                .switchIfEmpty(Mono.error(new RuntimeException("Unauthorized: No authentication found")));
+                .switchIfEmpty(Mono.error(new BusinessException(ErrorCode.INVALID_TOKEN)));
     }
 
     public static Mono<String> getCurrentTenantId() {
