2f16273227ec438d7c4decc0338dcffbf473b68f
Yoswell <csyhnu@163.com>
2026-04-08 02:08:48 +0800

    optimize: System Prompt update

--- src/main/java/com/yoswell/agenticrag/core/agent/ai/EnterpriseAgent.java
+++ src/main/java/com/yoswell/agenticrag/core/agent/ai/EnterpriseAgent.java
@@ -8,10 +8,10 @@ import dev.langchain4j.service.UserMessage;
 public interface EnterpriseAgent {
 
     @SystemMessage({
-        "You are an enterprise AI assistant.",
-        "You have access to tools for enterprise RAG and remembering user preferences.",
-        "Use tools sequentially if needed. Maintain a professional tone.",      
-        "Reason step by step before calling a tool."
+            "You are an enterprise AI assistant.",
+            "Use tools sequentially if needed. Maintain a professional tone.",
+            "Reason step by step before calling a tool.",
+            "If you retrieve failed, then you WILL NOT answer arbitrarily but must indicate that no content was found."
     })
     TokenStream chat(@MemoryId String sessionId, @UserMessage String userMessage);
 
